62ccf61bf7677f62805640597dafb531bbf6ead1
whcarroll <william.h.carroll@gmail.com>
2018-12-01 11:48:46 -0600

    hi

---
+++ pong-will.py
@@ -0,0 +1,45 @@
+import pygame
+
+pygame.init()
+width = 1200
+height = 600
+
+WHITE =     (255, 255, 255)
+BLUE =      (  0,   0, 255)
+GREEN =     (  0, 255,   0)
+RED =       (255,   0,   0)
+
+screen = pygame.display.set_mode((width, height))
+
+running = True
+
+class Ball:
+	def __init__(self):
+		self.x = 50
+		self.y = 50
+
+	def updatePosition(self):
+		self.x += 1
+		self.y += 1
+
+	def getPosition(self):
+		return (self.x,self.y)
+
+b = Ball()
+
+def getPos():
+    pos = pygame.mouse.get_pos()
+    return (pos)
+
+def drawCircle():
+    pos= b.getPosition()
+    b.updatePosition()
+    pygame.draw.circle(screen, BLUE, pos, 20)
+
+while running:
+        ev = pygame.event.get()
+        screen.fill(GREEN)
+        drawCircle()
+        pygame.display.update()
+
+
